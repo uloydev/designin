aaec12fb6f1c5227dbfe09f92e61aa435e25ff69
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2020-05-07 06:25:02 +0700

    nothing

--- app/Http/Controllers/Agent/AgentController.php
+++ app/Http/Controllers/Agent/AgentController.php
@@ -11,8 +11,10 @@ class AgentController extends Controller
 {
     public function profile()
     {
-        $listBank = public_path('js/bank_indonesia.json');
-        $listBank = json_decode($listBank);
+//        $listBank = public_path('js/bank_indonesia.json');
+//        $listBank = '{"label": "Bank Mandiri","value": "mandiri","label": "Bank Rakyat Indonesia","value": "bri","label": "BNI (Bank Negara Indonesia) & BNI Syariah","value": "bni"}';
+        $listBank = public_path("js/test.json");
+        $listBank = json_decode($listBank, true);
         $profile = User::findOrFail(Auth::id());
 //        $osi = '{"physical":"cables","data link":"mac address","network":"ip address","transport":"tcp","session":"application connections","presentation":"translation","application":"email"}';
 //        $osi = json_decode($osi);
