5772527947a2f55fd3422131a5d4b68594e4525c
uloydev <uloydev@gmail.com>
2020-05-27 19:07:21 +0700

    [FIX] artisan serve

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
