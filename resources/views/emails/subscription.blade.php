2d7215eafcca12c2cb48524395734c5bbd5c9850
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-06-28 01:53:01 +0700

    [UPDATE] subscription mail

--- resources/views/emails/subscription.blade.php
+++ resources/views/emails/subscription.blade.php
@@ -1,1 +1,8 @@
-# notification
+@component('mail::message')
+    # Your subscription for {{ $order->package->title }}
+    Thank you for your subscription. For better communication about this subscription,
+    please contact admin at number [087776196047](https://wa.web/6287776196047)
+
+    Thank you for your order, <br>
+    {{ config('app.name') }}
+@endcomponent
