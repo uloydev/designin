533c3f89c0b84d6c2d5e4d19dfa40b5690eadf5c
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-06-28 02:12:13 +0700

    [FIX] subscription order mail

--- resources/views/emails/subscription.blade.php
+++ resources/views/emails/subscription.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-    # Your subscription for {{ $order->package->title }}
+    # Your subscription for {{ $order->subscription->title }}
     Thank you for your subscription. For better communication about this subscription,
     please contact admin at number [087776196047](https://wa.web/6287776196047)
 
